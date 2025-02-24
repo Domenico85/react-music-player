1b431c19afce3522deeee7c4a4065ec703ac2c68
Dome <arillotta@gmail.com>
2025-02-24 15:56:54 +0100

    update server.js

--- server.js
+++ server.js
@@ -3,21 +3,18 @@ import axios from "axios";
 import cors from "cors";
 import dotenv from "dotenv";
 
-// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-const PORT = 5000; // Backend port
+const PORT = 5000;
 
-// CORS configuration
 const corsOptions = {
-  origin: "http://localhost:5173", // Frontend URL (for Vite)
+  origin: "http://localhost:5173",
   methods: "GET",
   allowedHeaders: "Content-Type",
 };
-app.use(cors(corsOptions)); // Enable CORS
+app.use(cors(corsOptions));
 
-// Define the /api/search route
 app.get("/api/search", async (req, res) => {
   const { track } = req.query;
   const API_KEY = process.env.LASTFM_API_KEY;
@@ -26,19 +23,18 @@ app.get("/api/search", async (req, res) => {
     const response = await axios.get("https://ws.audioscrobbler.com/2.0/", {
       params: {
         method: "track.search",
-        track, // The search term (track name)
+        track,
         api_key: API_KEY,
         format: "json",
       },
     });
-    res.json(response.data); // Send data back to frontend
+    res.json(response.data);
   } catch (error) {
     console.error("Error fetching tracks from Last.fm:", error);
     res.status(500).json({ error: "Error fetching data" });
   }
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
